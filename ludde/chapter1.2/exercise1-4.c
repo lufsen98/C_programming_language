ea0b1938d32a9af4a7eea731c25410a5945f0f54
Ludde <luddenorin@gmail.com>
2025-04-03 13:36:48 +0200

    made some formating fixes

--- ludde/chapter1.2/exercise1-4.c
+++ ludde/chapter1.2/exercise1-4.c
@@ -6,7 +6,7 @@ int main() {
     int max_value = 300;
     int step_over = 20;
 
-    printf("Celsius to Farenheit\n");
+    printf("%7s|%10s\n%17s\n","Celsius","Farhenheit","---------------------------");
     while(celsius <= max_value) {
         farh = celsius*(9.0/5.0)+32;
         printf("%d\t%6.1f\n",celsius,farh);
